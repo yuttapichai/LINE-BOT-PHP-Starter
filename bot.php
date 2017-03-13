7fa2019d8a3252bc6e3ea0a17271283a730e8ee2
yuttapichai <yuttapichai.leelai@gmail.com>
2017-03-13 23:08:32 +0700

    version 0.1.0

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
