a88f76688581bdaaa2f72a11025fcc0f3a441b48
yuttapichai <yuttapichai.leelai@gmail.com>
2017-03-13 23:03:31 +0700

    version 0.1.3

--- bot.php
+++ bot.php
@@ -12,14 +12,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text'
-				// 'text' => $text
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
