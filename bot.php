195e3ae70b05e956febe23a9f96ab6bc4aeeaa94
yuttapichai <yuttapichai.leelai@gmail.com>
2017-03-13 22:59:00 +0700

    version 0.1.2

--- bot.php
+++ bot.php
@@ -18,8 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				// 'type' => 'text',
-				'text' => $text
+				'type' => 'text'
+				// 'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
